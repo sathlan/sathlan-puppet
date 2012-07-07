8f096626dd1ba3328580afa95df9e519aaf2a298
sathlan <mypublicaddress-code@ymail.com>
2012-07-07 19:27:19 +0200

    Add and adjust tests.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe 'puppet' do
-  let(:params) { {:dbadapter => 'sqlite3'} }
-  let(:facts) { {:fqdn => 'test.example.com'}}
-  it {should include_class('puppet::config')}
-  it {should include_class('puppet::install')}
-  it {should include_class('puppet::service')}
-  it {should include_class('puppet::params')}
+  describe "Default sqlite3 configuration." do
+    let(:params) { {:dbadapter => 'sqlite3'} }
+    let(:facts) { {:fqdn => 'test.example.com'}}
+    it {should include_class('puppet::config')}
+    it {should include_class('puppet::install')}
+    it {should include_class('puppet::service')}
+    it {should include_class('puppet::params')}
 
-  it { should contain_package('libactiverecord-ruby1.8') }
-  it { should contain_package('libsqlite3-ruby1.8') }
-  it { should contain_package('sqlite3') }
-  it { should contain_file('/etc/puppet/puppet.conf') \
-      .with_content(/dbadapter .*sqlite3/).with_content(/test.example.com.cert/)}
+    it { should contain_package('libactiverecord-ruby1.8') }
+    it { should contain_package('libsqlite3-ruby1.8') }
+    it { should contain_package('sqlite3') }
+    it { should contain_file('/etc/puppet/puppet.conf') \
+        .with_content(/dbadapter .*sqlite3/).with_content(/test.example.com.cert/) \
+        .with_content(/modulepath .*environments\/production\/modules/)}
+  end
+  describe "Modulepath is set correctly" do
+    let(:params) { {:prod_modules => '/tmp/test'} }
+    it { should contain_file('/etc/puppet/puppet.conf') \
+        .with_content(/modulepath .*\/tmp\/test/)}
+  end
 end
