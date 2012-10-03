002dda8a6a45e3dd2750283cfe9681ab111f78fb
sathlan <mypublicaddress-code@ymail.com>
2012-10-03 18:44:21 +0200

    Fix startup option for puppetmaster.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,14 +1,29 @@
-class puppet::service ($use_passenger = false) {
+class puppet::service ($use_passenger = false, $daemon_opts = '') {
   if ($use_passenger) {
     $ensure = stopped
+    $start  = 'NO'
+    $service = 'apache2'
+    $enable  = false
   } else {
     $ensure = running
+    $start  = 'YES'
+    $service = 'puppetmaster'
+    $enable  = true
   }
-  service { 'puppetmaster':
+  service { "puppetmaster":
     ensure => $ensure,
     hasstatus => true,
     hasrestart => true,
-    enable  => true,
+    enable  => $enable,
     require => Class['puppet::install'],
+  }
+  if ($::osfamily == 'Debian') {
+    file {'/etc/default/puppetmaster':
+      content => template('puppet/default-puppetmaster.erb'),
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0544',
+      require => Class['puppet::install'],
+    }
   }
 }
