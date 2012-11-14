98df241a6c3401b00300ee45724d44b9a5b09d22
sathlan <chem@sathlan.org>
2012-11-14 10:10:10 +0100

    fix certname default value.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class puppet::params {
   $puppetserver = "$::fqdn"
   $module_base_path = '/etc/puppet/environments'
-  $certname_default = "${::fqdn}.cert"
+  $certname_default = "${::fqdn}"
   $prod_modules_default = "${module_base_path}/production/modules"
   $prod_manifest_default = "${module_base_path}/production/manifests/site.pp"
 }
