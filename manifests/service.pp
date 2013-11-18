b86940935fd89224e99c4ea01a2144ac8d7f14e7
sathlan <chem@sathlan.org>
2013-11-18 10:35:46 -0500

    Remove useless service.

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,20 +10,4 @@ class puppet::service ($use_passenger = false, $daemon_opts = '') {
     $service = 'puppetmaster'
     $enable  = true
   }
-  service { "puppetmaster":
-    ensure => $ensure,
-    hasstatus => true,
-    hasrestart => true,
-    enable  => $enable,
-    require => Class['puppet::install'],
-  }
-  if ($::osfamily == 'Debian') {
-    file {'/etc/default/puppetmaster':
-      content => template('puppet/default-puppetmaster.erb'),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0544',
-      require => Class['puppet::install'],
-    }
-  }
 }
